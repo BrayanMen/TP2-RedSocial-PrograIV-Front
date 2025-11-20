f53af0fc09b3627d1d415655644b203316cdf2d8
BrayanMen <brayanjmr880@gmail.com>
2025-11-20 00:51:34 -0300

    add services and interface comments

--- src/app/core/interfaces/comment.interface.ts
+++ src/app/core/interfaces/comment.interface.ts
@@ -10,6 +10,26 @@ export interface IComment {
   };
   content: string;
   isModified: boolean;
-  createdAt: Date;
-  updatedAt: Date;
+  createdAt: Date | string;
+  updatedAt: Date | string;
+}
+
+export interface ICreateCommentRequest {
+  content: string;
+}
+
+export interface IUpdateCommentRequest {
+  content: string;
+}
+
+export interface ICommentsResponse {
+  data: IComment[];
+  meta: {
+    total: number;
+    page: number;
+    limit: number;
+    totalPages: number;
+    nextPage: boolean;
+    prevPage: boolean;
+  };
 }
--- src/app/core/services/comments-service.ts
+++ src/app/core/services/comments-service.ts
@@ -1,8 +1,48 @@
 import { Injectable } from '@angular/core';
+import { ApiService } from './api-service';
+import { map, Observable } from 'rxjs';
+import {
+  IComment,
+  ICommentsResponse,
+  ICreateCommentRequest,
+  IUpdateCommentRequest,
+} from '../interfaces/comment.interface';
+import { HttpParams } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CommentsService {
-  
+  private readonly postsUrl = 'posts/';
+
+  constructor(private apiService: ApiService) {}
+
+  getComments(page: number = 1, limit: number = 5, postId: string): Observable<ICommentsResponse> {
+    let params = new HttpParams().set('page', page).set('limit', limit);
+    return this.apiService
+      .get<ICommentsResponse>(`${this.postsUrl}${postId}/comments`, params)
+      .pipe(map((c) => c.data));
+  }
+
+  createComment(comment: ICreateCommentRequest, postId: string): Observable<IComment> {
+    return this.apiService
+      .post<IComment>(`${this.postsUrl}${postId}/comments`, comment)
+      .pipe(map((c) => c.data));
+  }
+
+  deleteComment(postId: string, commentId: string): Observable<{ message: string }> {
+    return this.apiService
+      .delete<{ message: string }>(`${this.postsUrl}${postId}/comments/${commentId}`)
+      .pipe(map((r) => r.data));
+  }
+
+  updatedComment(
+    postId: string,
+    commentId: string,
+    contentUpdated: IUpdateCommentRequest
+  ): Observable<IComment> {
+    return this.apiService
+      .put<IComment>(`${this.postsUrl}${postId}/comments/${commentId}`,contentUpdated)
+      .pipe(map((r) => r.data));
+  }
 }
