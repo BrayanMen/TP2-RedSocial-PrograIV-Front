7655304635287bc9ac4edc1a3ba15673b53b6c33
BrayanMen <brayanjmr880@gmail.com>
2025-11-21 13:16:46 -0300

    Sprint 3 - fixed serverRoutes

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'posts/:id',
+    renderMode: RenderMode.Server,
+  },
+  { path: 'profile/:id', renderMode: RenderMode.Server },
+  {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
 ];
